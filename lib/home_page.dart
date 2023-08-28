e60635a07c65698314efe5626ef1422336b3e9e8
0-yash-sharma-0 <yasharma999999@gmail.com>
2023-08-28 21:04:12 +0530

    day1 completed

---
+++ lib/home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text("Catalog App")),
+      ),
+      body: Center(child: Container(child: Text("First Flutter App day"))),
+      drawer: Drawer(),
+    );
+  }
+}
